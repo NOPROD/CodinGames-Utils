ae257ce20cb8e2d13f5df5f2b4b60b3dd649ea0f
NOPROD <contact@boucham-amine.fr>
2020-08-27 06:19:48 +0200

    feat: better billChange

--- billChange.java
+++ billChange.java
@@ -1,30 +1,42 @@
-public class change {
+public class Change {
     long coin2 = 0;
     long bill5 = 0;
     long bill10 = 0;
 }
 
 class Solution {
-    static change optimalChange(long s) {
-        change c = new change();
-        if (s >= 10) {
-            c.bill10 = s / 10;
-            if (s % 10 >= 5) {
-                c.bill5 = (s % 10) / 5;
-                if ((s % 10) % 5 >= 2) {
-                    c.coin2 = ((s % 10) % 5) / 2;
-                }
-            } else if (s % 10 >= 2) {
-                c.coin2 = (s % 10) / 2;
+
+    static Change optimalChange(long changeDue) {
+        long NbrOfBill10 = 0;
+        long NbrOfBill5 = 0;
+        long NbrOfCoin2 = 0;
+
+        long remaining = changeDue;
+
+        NbrOfBill10 = Math.round(remaining / 10);
+        remaining = remaining % 10;
+
+        NbrOfBill5 = Math.round(remaining / 5);
+        remaining = remaining % 5;
+
+        if (remaining < 2) {
+            remaining = (NbrOfBill5 * 5) + 1;
+            if (remaining % 2 == 0) {
+                NbrOfBill5 = 0;
+                NbrOfCoin2 = Math.round(remaining / 2);
             }
-        } else if (s >= 5 && s < 10) {
-            c.bill5 = s / 5;
-            if (s % 5 >= 2) {
-                c.coin2 = (s % 5) / 2;
-            }
-        } else if (s > 2 && s < 5) {
-            c.coin2 = s / 2;
+        } else {
+            NbrOfCoin2 = Math.round(remaining / 2);
         }
-        return c;
+        remaining = remaining % 2;
+
+        if (remaining != 0)
+            return null;
+
+        Change change = new Change();
+        change.coin2 = NbrOfCoin2;
+        change.bill5 = NbrOfBill5;
+        change.bill10 = NbrOfBill10;
+        return change;
     }
 }
